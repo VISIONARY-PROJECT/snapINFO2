50d0c813fc66138d7d9fb2cb75a970c2be8ff9d9
민재준 <minjaejun291@gmail.com>
2024-11-26 13:50:17 +0900

    2v-3(어디서 오류나는지 파악)

--- text_model.py
+++ text_model.py
@@ -22,26 +22,31 @@ def text_out(path):
     
     return combined_text
 
+# koBART 모델과 토크나이저 불러오기
+tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
+model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
+print("model download")
+
 # 텍스트 파일 경로 (바탕화면에 위치한 txt 파일 경로를 설정)
 def summary_text(text):
-    # koBART 모델과 토크나이저 불러오기
-    tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
-    model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
-
     # 텍스트 요약 처리
     raw_input_ids = tokenizer.encode(text)
     input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
+    print("텍스트 요약 처리 1단계")
 
     summary_ids = model.generate(torch.tensor([input_ids]))
+    print("2단계")
     # 요약 생성 (최소 길이 설정)
     summary_ids = model.generate(
         torch.tensor([input_ids]),
         min_length=50,  # 요약의 최소 길이 (50 토큰)
         max_length=200,  # 요약의 최대 길이 (200 토큰)
         length_penalty=2.0,  # 길이에 대한 페널티 (길이를 조정할 때 유용)
-        num_beams=4  # 빔 서치 사용 (더 나은 요약 생성 가능)
+        num_beams=1  # 빔 서치 사용 (더 나은 요약 생성 가능)
     )
+    print("3단계")
     summary = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+    print("4단계")
 
     # 요약 결과 출력
     print("요약 결과:")
